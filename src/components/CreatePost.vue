d6651e574c2d23893bf3ad11b9dfe8b9d2115faa
waikau maina <ericmaina.dev@gmail.com>
2023-05-16 15:38:20 +0300

    created a form

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-
+    <form>
+      <div>
+        <label for="userId">Post User Id</label>
+        <input type="text" name="userId" id="userId" v-model="formData.userId">
+      </div>
+      <div>
+        <label for="title">Post title</label>
+        <input type="text" name="title" id="title" v-model="formData.title">
+      </div>
+      <div>
+        <label for="body">Post Body</label>
+        <input type="text" name="body" id="body" v-model="formData.body">
+      </div>
+      <button>Create Post</button>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
-    name: "CreatePost"
+    name: "CreatePost",
+    data() {
+      return {
+        formData: {
+          userId: "",
+          title: "",
+          body: ""
+        }
+      }
+    }
   }
 </script>
 
